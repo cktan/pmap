8eebf1475cf0d9888e79b46416f6b9efa37b03e9
CK Tan <cktan@vitessedata.com>
2019-11-20 00:08:48 -0800

    minor

--- src/pmaptest/pmaptest.go
+++ src/pmaptest/pmaptest.go
@@ -5,19 +5,32 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"os"
 )
 
 
-func work(k int) {
-	x := rand.Intn(10)
-	for i := 0; i < x; i++ {
-		fmt.Println(k, "sleep", x - i)
-		time.Sleep(time.Second)
-	}
-	fmt.Println(k, "wake")
-}
 
 func main() {
+	
+	var status [20]int
+	
+	work := func(k int) {
+		fmt.Println(k, "start")
+		x := rand.Intn(10)
+		for i := 0; i < x; i++ {
+			fmt.Println(k, "sleep", x - i)
+			time.Sleep(time.Second)
+		}
+		fmt.Println(k, "fin")
+		status[k] = 999
+	}
 	pmap.Pmap(work, 20, 2)
+
+	for i := 0; i < 20; i++ {
+		if status[i] != 999 {
+			fmt.Println("ERROR");
+			os.Exit(1)
+		}
+	}
 }
 
