7767b04bcb30b3bb78486b3f63352195ab21661c
CK Tan <cktan@vitessedata.com>
2019-11-27 16:31:50 -0800

    clean

--- src/pmap/pmap.go
+++ src/pmap/pmap.go
@@ -1,5 +1,7 @@
 package pmap
 
+import "sync"
+
 
 /**
  *  Process N items using M go routines
@@ -9,54 +11,31 @@ func Pmap(processItem func(n int), N int, M int) {
 		M = N
 	}
 
-	var fin chan int
-	var ticket chan int
-	if false {
-		// for debug
-		fin = make(chan int)
-		ticket = make(chan int)
-	} else {
-		fin = make(chan int, 10)
-		ticket = make(chan int, 10)
-	}
+	wg := &sync.WaitGroup{}
+	ticket := make(chan int, 10)
 
 	// let M workers run concurrently
+	wg.Add(M)
 	for i := 0; i < M; i++ {
 		go func() {
 			for {
 				idx, ok := <-ticket
 				if !ok {
-					return
+					// no more ticket. sender closed.
+					break
 				}
 				processItem(idx)
-				fin <- idx
 			}
+			wg.Done()
 		}()
 	}
 
-	// send the jobs. Do this in a go routine so we don't have a
-	// race between ticket and fin
-	go func() {
-		for i := 0; i < N; i++ {
-			ticket <- i
-		}
-		close(ticket)
-	}()
+	// send the jobs
+	for i := 0; i < N; i++ {
+		ticket <- i
+	}
+	close(ticket)
 
 	// wait for all jobs to finish
-	for i := 0; i < N; i++ {
-		<-fin
-	}
-
-	// Go Channel Closing Principle:
-	//  - Don't close a channel from the receiver side and
-	//  - Don't close a channel if the channel has multiple concurrent senders.
-
-	// At this point, we gave out N tickets, and we received N fins.
-	// All workers must be blocked waiting for ticket at this time.
-	// They will next get the term signal and exit.
-	
-	// Even though we are receiver on fin, we can close it making sure
-	// sure that no one will ever send to it again.
-	close(fin)
+	wg.Wait()
 }
